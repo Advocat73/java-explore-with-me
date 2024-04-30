430827507dfe142bc36715ad43457eff5a9637d0
Toropov Alex <toropov_alex@mail.ru>
2024-04-30 18:20:43 +0200

    ДП: Часть вторая. Версия первая

--- stats/stats-server/src/main/java/ru/practicum/ewm/stats/endpointRequest/EndpointRequestController.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/stats/endpointRequest/EndpointRequestController.java
@@ -7,11 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpClientErrorException;
 import ru.practicum.ewm.stats.endpointRequestDto.EndpointHit;
 import ru.practicum.ewm.stats.endpointRequestDto.ViewStats;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
@@ -25,35 +23,23 @@ public class EndpointRequestController {
 
     @PostMapping("/hit")
     public ResponseEntity<EndpointHit> addNewRequest(@RequestBody EndpointHit endpointHit) {
-            //public EndpointHit addNewRequest(@RequestBody EndpointHit endpointHit) {
         log.info("ENDPOINT_REQUEST_SERVER_КОНТРОЛЛЕР: POST-запрос по эндпоинту /hit");
         return ResponseEntity.status(HttpStatus.CREATED).body(endpointRequestService.addNewEndpointRequest(endpointHit));
-        //return endpointRequestService.addNewEndpointRequest(endpointHit);
     }
 
     @GetMapping("/stats")
     public ResponseEntity<ViewStats[]> getEndpointRequestList(@NotNull(message = "Не указано время начала поиска")
-                                                          @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
-                                                          @RequestParam LocalDateTime start,
-                                                          @NotNull(message = "Не указано время конца поиска")
-                                                          @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
-                                                          @RequestParam LocalDateTime end,
-                                                          @RequestParam(required = false) String[] uris,
-                                                          @RequestParam(defaultValue = "false", required = false) Boolean unique) {
+                                                              @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+                                                              @RequestParam LocalDateTime start,
+                                                              @NotNull(message = "Не указано время конца поиска")
+                                                              @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+                                                              @RequestParam LocalDateTime end,
+                                                              @RequestParam(required = false) String[] uris,
+                                                              @RequestParam(defaultValue = "false", required = false) Boolean unique) {
         log.info("ENDPOINT_REQUEST_SERVER_КОНТРОЛЛЕР: GET-запрос по эндпоинту /stats?start={}&end={}&uris={}&unique={}", start, end, uris, unique);
         if (start.isAfter(end))
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        //ViewStats[] viewStats = endpointRequestService.findEndpointRequestList(start, end, uris, unique);
-        //ResponseEntity<ViewStats[]> rvs = ResponseEntity.status(HttpStatus.OK).body(viewStats);
-        //return rvs;
         return ResponseEntity.status(HttpStatus.OK).body(endpointRequestService.findEndpointRequestList(start, end, uris, unique));
     }
-
-   /* @GetMapping("/stats")
-    public ResponseEntity<Integer> getEndpointRequestList() {
-        Integer i = 5;
-        ResponseEntity<Integer> r = ResponseEntity.status(HttpStatus.OK).body(5);
-        return r;
-    }*/
 }
 
