b38fcd54d5d037366037bf8c8991fec1a77e35b3
Toropov Alex <toropov_alex@mail.ru>
2024-05-06 15:55:56 +0200

    ДП: часть третья. Первый выпуск

--- main-service/src/main/java/ru/practicum/ewm/eventlikes/EventLikeController.java
+++ main-service/src/main/java/ru/practicum/ewm/eventlikes/EventLikeController.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.ewm.events.EventService;
 
 @Slf4j
 @RestController
